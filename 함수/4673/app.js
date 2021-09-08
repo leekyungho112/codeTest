4b6443eb8a95bd27dd365a5c4f40bcf4c8c0be8b
kyungho <epsoe@naver.com>
2021-09-08 11:32:57 +0900

    6일차

--- 함수/4673/app.js
+++ 함수/4673/app.js
@@ -1,24 +1,29 @@
 function d(num) {
   let sum = 0;
   const strNum = String(num);
+  console.log('str', strNum);
   for (let i = 0; i < strNum.length; i++) {
     sum += +strNum[i];
+    console.log('sum:', sum);
   }
-  console.log(sum);
+  console.log('N+S', num + sum);
   return num + sum;
 }
 
 function solution() {
-  const arr = Array(10001).fill(0);
-  for (let i = 1; i <= 10000; i++) {
+  const arr = new Array(101).fill(0);
+  console.log('arr', arr);
+  for (let i = 1; i <= 100; i++) {
     const ans = d(i);
-    if (ans <= 10000) {
+    console.log('ans', ans);
+    if (ans <= 100) {
       arr[ans]++;
+      console.log('lastArr', arr);
     }
   }
-  for (let i = 0; i <= 10000; i++) {
+  for (let i = 1; i <= 100; i++) {
     if (arr[i] === 0) {
-      // console.log(i);
+      console.log('i', i);
     }
   }
 }
